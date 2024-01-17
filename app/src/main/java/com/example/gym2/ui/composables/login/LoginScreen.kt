424f91c651f5ca34ce463a8ee9d63b3f09fc4023
John Kenedy <john.kenedy0410@gmail.com>
2024-01-17 11:30:55 -0300

    LoginScreen.kt and ExercisesScreen.kt Done

--- app/src/main/java/com/example/gym2/ui/composables/login/LoginScreen.kt
+++ app/src/main/java/com/example/gym2/ui/composables/login/LoginScreen.kt
@@ -1,13 +1,28 @@
 package com.example.gym2.ui.composables.login
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Icon
 import androidx.compose.material.ScaffoldState
 import androidx.compose.material.SnackbarDuration
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.material.TextFieldDefaults.textFieldColors
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Lock
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -18,14 +33,27 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.gym2.ui.navigation.MAIN_ROUTE
 import com.example.gym2.ui.theme.veryDarkBlue
 import com.example.gym2.R
+import com.example.gym2.ui.composables.RegularButton
+import com.example.gym2.ui.composables.home.Heading
+import com.example.gym2.ui.navigation.Screens
 import com.example.gym2.ui.theme.holoGreen
+import com.example.gym2.ui.theme.outfit
+import com.example.gym2.ui.theme.white
 import com.example.gym2.viewmodel.UserViewModel
 
 @Composable
@@ -37,7 +65,7 @@ fun LoginScreen(
 
     val state = userViewModel.signInState
 
-    var email by remember { mutableStateOf("") }
+    var eMail by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
 
     LaunchedEffect(key1 = state.error) {
@@ -84,8 +112,118 @@ fun LoginScreen(
                 horizontalAlignment = Alignment.Start
             ) {
 
+                Heading(
+                    text = stringResource(id = R.string.login),
+                    modifier = Modifier
+                        .align(Alignment.Start)
+                        .padding(bottom = 20.dp)
+                        .fillMaxWidth()
+                )
+
+                InputField(
+                    eMail,
+                    onValueChange = { eMail = it },
+                    placeholder = stringResource(id = R.string.email),
+                    icon = Icons.Rounded.Email,
+                    type = KeyboardType.Email
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+
+                InputField(
+                    input = password,
+                    onValueChange = { password = it },
+                    placeholder = stringResource(id = R.string.password),
+                    icon = Icons.Rounded.Lock,
+                    type = KeyboardType.Password,
+                    true
+                )
+
+                SignUpRow(
+                    modifier = Modifier
+                        .padding(top = 10.dp)
+                        .fillMaxWidth()
+                        .clickable { navController.navigate(Screens.Signup.route) }
+                )
+
+                RegularButton(
+                    Modifier
+                        .padding(top = 20.dp)
+                        .align(Alignment.CenterHorizontally),
+                    stringResource(id = R.string.login).lowercase(),
+                    onClick = {
+                        userViewModel.signInUser(
+                            userEmail = eMail,
+                            userPassword = password
+                        )
+
+                        eMail = ""
+                        password = ""
+                    }
+                )
+
             }
         }
 
     }
+}
+
+@Composable
+fun InputField(
+    input: String = "",
+    onValueChange: (String) -> Unit,
+    placeholder: String = "email",
+    icon: ImageVector = Icons.Rounded.Email,
+    type: KeyboardType = KeyboardType.Email,
+    password: Boolean = false
+) {
+
+    TextField(
+        value = input,
+        onValueChange = onValueChange,
+        leadingIcon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = null,
+
+            )
+
+        },
+        colors = textFieldColors(backgroundColor = white, textColor = Color.Black),
+        label = { Text(text = placeholder) },
+        modifier = Modifier.fillMaxWidth(),
+        maxLines = 1,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = type,
+            imeAction = ImeAction.Done
+        ),
+        visualTransformation = if (password) PasswordVisualTransformation() else {
+            VisualTransformation.None
+        }
+    )
+
+}
+
+@Composable
+fun SignUpRow(modifier: Modifier = Modifier) {
+
+    Row(
+       verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = R.string.sign_up_text),
+            fontWeight = FontWeight.Normal,
+            color = white,
+            fontFamily = outfit
+        )
+        Spacer(modifier = Modifier.width(5.dp))
+
+        Text(
+            text = stringResource(id = R.string.sign_up),
+            fontWeight = FontWeight.Bold,
+            color = holoGreen,
+            fontFamily = outfit
+        )
+    }
+
 }
